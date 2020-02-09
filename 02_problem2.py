bdd1b2d6f1356a48db73895fb87ec945644c8dbf
Kimberly Ayala <KimAyala@KimBook.local>
2020-02-09 18:51:37 -0500

    add problem 2 from assignment 2

---
+++ 02_problem2.py
@@ -0,0 +1,14 @@
+# Tests whether one number is greather than another number
+# Kimberly Ayala, Feb 6 2020
+
+# Establish variables a and b and assign values to both
+a = 5
+b = 10
+
+# Test if a is greater than b
+# If it is, display 'a is greater than b'
+# Otherwise, display 'a is not greater than b'
+if a > b:
+	print(a, 'is greater than', b)
+else:
+	print(a, 'is not greater than', b)
