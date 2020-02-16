318179a6c2c8f5f585dad43c62ac0d7af3410704
Kimberly Ayala <KimAyala@KimBook.local>
2020-02-16 15:05:04 -0500

    add python script for assignment 3, problem 2

---
+++ 03_problem2.py
@@ -0,0 +1,11 @@
+# Establish variables a and b
+a = 1
+b = 3
+
+# Execute lines 7 and 8 three times
+# For each loop, update values of a and b
+for i in range(3):
+    a = b
+    b = a + 1
+
+print('What are the values of a and b?')
