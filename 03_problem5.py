5539a16d82a6a0b7a163d2d9b45e78ea28ba0e56
Kimberly Ayala <KimAyala@KimBook.local>
2020-02-17 12:14:53 -0500

    add python script for assignment 3, problem 5

---
+++ 03_problem5.py
@@ -0,0 +1,14 @@
+my_set = {'Homer','Marge'}
+
+# Add 'Bart' to my_set
+my_set.add('Bart')
+
+# Add 'Lis' to my_set
+my_set.add('Lisa')
+
+# Add 'Maggie' to my_set
+my_set.add('Maggie')
+
+# Add list (containing 'Bart' and 'Lisa') to my_set and print values
+my_set.update(['Bart','Lisa'])
+print(my_set)
