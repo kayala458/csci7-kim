583d6bd5c5b5de682559b32954a44768a23bcab5
Kimberly Ayala <KimAyala@KimBook.local>
2020-02-17 11:26:52 -0500

    add python script for assignment 3, problem 4

---
+++ 03_problem4.py
@@ -0,0 +1,20 @@
+# Establish items in 'my_tuple'
+my_tuple = ('Kimberly', 'Coral', 'Ayala')
+
+# Store all items from my_tuple in 'new_tuple'
+new_tuple = my_tuple
+
+# Store first item from 'my_tuple' in 'new_tuple'
+new_tuple = my_tuple[0]
+
+# Store last item from 'my_tuple' in 'new_tuple'
+new_tuple = my_tuple[-1]
+
+# Store second and third items from 'my_tuple' in 'new_tuple'
+new_tuple = my_tuple[1:3]
+
+# Store first and second items from 'my_tuple' in 'new_tuple'
+new_tuple = my_tuple[-3:-1]
+
+# New line added to show what is stored in new_tuple after line 17 in debugger
+print(new_tuple)
