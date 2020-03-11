2fc49339a81ee5117776d0f279d61abe0e345c86
kayala458 <kcaayala@gmail.com>
2020-03-10 20:43:19 -0400

    Base pair counter script for Assignment 5, Problem 2

--- problem2_base_pair_script.py
+++ problem2_base_pair_script.py
@@ -4,10 +4,12 @@ import problem2_base_pairs as pair_counter
 # Prompt user for file name
 n = input("Please provide the name of a fasta file: ")
 
-print('The result is:')
-
 # Count the number of base pairs found in the file
 pairsCount = pair_counter.count_digram(n)
 
-# Print the total count of each pair in the form of a digram
-pair_counter.printDigrams(pairsCount)
+if pairsCount:
+
+    print('The result is:')
+
+    # Print the total count of each pair in the form of a digram
+    pair_counter.printDigrams(pairsCount)
